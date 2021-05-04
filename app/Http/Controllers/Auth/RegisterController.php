5696b3412edd29637ad7b8f947c8e8d918adf57b
shulga508 <y.shulga393@gmail.com>
2021-05-05 01:06:19 +0300

    refactor register controller

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -9,6 +9,8 @@ use App\UserList;
 use Illuminate\Http\Request;
 use App\User;
 use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\DB;
 
 class RegisterController extends Controller
 {
@@ -20,65 +22,53 @@ class RegisterController extends Controller
     public function create(Request $request)
     {
         $data = $request->all();
-        if (!empty($data['email']) && !empty($data['password']) && !in_array($this->getIpAddress(),
-                self::DISABLED_IPS)) {
-            $emailParts = explode('@', $data['email']);
-            if (!$this->is_banned_domain($emailParts[1])) {
+        $validator = Validator::make($data, [
+            'email' => 'required|email|unique:user|max:255',
+            'password' => 'required',
+        ]);
+
+        if ($validator->fails() ||
+            in_array($request->ip(), self::DISABLED_IPS) ||
+            $this->isBannedDomain(substr($data['email'], strpos($data['email'], '@')))
+        ) {
+            return $this->returnErrorResponse();
+        }
+
+        try {
+            DB::transaction(function () use ($data) {
                 $user = new User();
                 $user->name = $data['name'];
                 $user->email = $data['email'];
                 $user->password = bcrypt($data['password']);
                 $user->save();
 
-                if ($user) {
-                    Mail::to($user)->send(new ConfirmationEmail());
-
-                    $user_list = new UserList();
-                    $user_list->user_id = $user->id;
-                    $user_list->name = 'First email addresses list';
-                    $user_list->save();
-
-                    file_put_contents(storage_path("logs/registration-success" . date('Y-m-d') . '.log'),
-                        print_r($data, true), FILE_APPEND | LOCK_EX);
-
-                    return response()->json('ok', 500);
-                }
-            }
+                $user_list = new UserList();
+                $user_list->user_id = $user->id;
+                $user_list->name = 'First email addresses list';
+                $user_list->save();
+                Mail::to($user)->send(new ConfirmationEmail());
+            });
+        } catch (\Exception $e) {
+            return $this->returnErrorResponse();
         }
 
+        file_put_contents(storage_path("logs/registration-success" . date('Y-m-d') . '.log'),
+            print_r($data, true), FILE_APPEND | LOCK_EX);
+
+        return response()->json('ok', 200);
+
+    }
+
+
+    private function isBannedDomain($domain): bool
+    {
+        return in_array($domain, BannedDomain::all()->toArray(), true);
+    }
+
+    private function returnErrorResponse() {
         file_put_contents(storage_path("logs/registration-error" . date('Y-m-d') . '.log'), print_r($data, true),
             FILE_APPEND | LOCK_EX);
 
         return response()->json('error', 500);
-    }
-
-    private function getIpAddress(): string
-    {
-        $ip = '';
-        foreach ([
-                     'HTTP_CF_CONNECTING_IP',
-                     'REMOTE_ADDR',
-                     'HTTP_CLIENT_IP',
-                     'HTTP_X_FORWARDED_FOR',
-                     'HTTP_X_FORWARDED',
-                     'HTTP_X_CLUSTER_CLIENT_IP',
-                     'HTTP_FORWARDED_FOR',
-                     'HTTP_FORWARDED'
-                 ] as $key) {
-            if (array_key_exists($key, $_SERVER) === true) {
-                foreach (explode(',', $_SERVER[$key]) as $ip) {
-                    if (filter_var($ip, FILTER_VALIDATE_IP) !== false) {
-                        return $ip;
-                    }
-                }
-            }
-        }
-
-        return $ip;
-    }
-
-    private function is_banned_domain($domain): bool
-    {
-        return in_array($domain, BannedDomain::all()->toArray(), true);
     }
 }
